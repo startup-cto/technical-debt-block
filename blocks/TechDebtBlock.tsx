34153e088445ac59ab8c66120c8ac027323d9975
Daniel Bartholomae <daniel@bartholomae.name>
2023-03-06 00:10:41 +0800

    Make list more beautiful

--- blocks/TechDebtBlock.tsx
+++ blocks/TechDebtBlock.tsx
@@ -1,5 +1,5 @@
 import { FolderBlockProps } from "@githubnext/blocks";
-import { Box } from "@primer/react";
+import { ActionList, Box } from "@primer/react";
 import dayjs from "dayjs";
 import { useEffect, useState } from "react";
 import { makeLoadTechDebt } from "./makeLoadTechDebt";
@@ -46,31 +46,19 @@ export default function TechDebtBlock(props: FolderBlockProps) {
           This is the folder content.
         </Box>
         <Box p={4}>
-          <table style={{ textAlign: "left" }}>
-            <thead>
-              <tr>
-                <th className="p-1">Path</th>
-                <th className="p-1">Commit count</th>
-                <th className="p-1">File size</th>
-                <th className="p-1">Complexity</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map(({ path, commitCount, size, complexity }) => (
-                <tr key={path}>
-                  <td
-                    className="p-1"
-                    onClick={() => props.onNavigateToPath(path)}
-                  >
-                    {path}
-                  </td>
-                  <td className="p-1">{commitCount}</td>
-                  <td className="p-1">{size}</td>
-                  <td className="p-1">{complexity}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <ActionList>
+            {data.map(({ path, commitCount, size, complexity }) => (
+              <ActionList.Item
+                key={path}
+                onClick={() => props.onNavigateToPath(path)}
+              >
+                {path}
+                <ActionList.TrailingVisual>
+                  {commitCount} commits, {size} kB size, {complexity} complexity
+                </ActionList.TrailingVisual>
+              </ActionList.Item>
+            ))}
+          </ActionList>
         </Box>
       </Box>
     </Box>
