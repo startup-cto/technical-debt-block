8b7b9cb766b398c1ca59982f48d2b8636c87fd43
Daniel Bartholomae <daniel@bartholomae.name>
2023-03-05 19:38:44 +0800

    Add linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,92 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2020: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  extends: ['eslint:recommended', 'plugin:import/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    // Too slow
+    'import/namespace': 'off',
+    'prettier/prettier': 'error',
+    'no-console': ['error'],
+    'no-else-return': ['error'],
+    'no-lonely-if': ['error'],
+    'no-nested-ternary': ['error'],
+    'no-param-reassign': ['error'],
+    'no-promise-executor-return': ['error'],
+    'no-return-assign': ['error'],
+    'no-return-await': ['error'],
+    'no-self-compare': ['error'],
+    'no-template-curly-in-string': ['error'],
+    'no-unmodified-loop-condition': ['error'],
+    'no-unneeded-ternary': ['error'],
+    'no-unreachable-loop': ['error'],
+    'no-useless-call': ['error'],
+    'no-useless-concat': ['error'],
+    'prefer-template': ['error'],
+    'object-shorthand': ['error'],
+    'prefer-destructuring': ['error'],
+    'require-atomic-updates': ['error']
+  },
+  overrides: [{
+    files: ['*.ts', '*.tsx'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: ['./tsconfig.json']
+    },
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking', 'plugin:import/typescript'],
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', {
+        destructuredArrayIgnorePattern: "^_",
+        argsIgnorePattern: "^_(?!id)"
+      }],
+      '@typescript-eslint/ban-ts-comment': ['error', {
+        'ts-ignore': 'allow-with-description',
+        minimumDescriptionLength: 10
+      }]
+    }
+  }, {
+    files: ['*.jsx', '*.tsx', '*/storybook/preview.js'],
+    extends: ["plugin:react/recommended", "plugin:react/jsx-runtime", "plugin:react-hooks/recommended"],
+    env: {
+      browser: true
+    },
+    rules: {
+      "react/jsx-uses-react": "error"
+    }
+  }, {
+    files: ['test/**', '*.test.*'],
+    extends: ['plugin:jest/recommended', 'plugin:jest/style', 'plugin:jest-formatting/strict'],
+    rules: {
+      'jest/expect-expect': ['error', {
+        // testing AWS CDK does not require to use an expect
+        // but has a custom hasResourceProperties matcher for cloud formation templates
+        assertFunctionNames: ['expect', '*.hasResourceProperties']
+      }]
+    }
+  }, {
+    files: ['test/**.ts', '**/*.test.ts', '**/*.test.tsx'],
+    rules: {
+      // Allow unbound methods in expect statements for testing spies
+      '@typescript-eslint/unbound-method': 'off',
+      'jest/unbound-method': 'error'
+    }
+  }],
+  settings: {
+    react: {
+      version: "detect"
+    },
+    'import/resolver': {
+      typescript: true,
+      node: true
+    }
+  }
+};
--- blocks/TechDebtBlock.tsx
+++ blocks/TechDebtBlock.tsx
@@ -1,67 +1,93 @@
-import {FolderBlockProps} from "@githubnext/blocks";
-import {Box} from "@primer/react";
-import {useEffect, useState} from "react";
+import { FolderBlockProps } from "@githubnext/blocks";
+import { Box } from "@primer/react";
+import { useEffect, useState } from "react";
 
 export default function TechDebtBlock(props: FolderBlockProps) {
-    const [data, setData] = useState<{ path: string, commitCount: number, size: number, complexity: number }[]>([])
-    useEffect(() => {
-        const {owner, repo} = props.context;
-        Promise.all(props.tree.filter(item => item.type === 'blob' && item.path?.match(/\.(tsx?|jsx?)$/)).map(async ({path = '/', size}) => ({
-            path,
-            size: size ?? 0,
-            commitCount: (await props.onRequestGitHubEndpoint('GET /repos/{owner}/{repo}/commits', {
-                path,
-                owner,
-                repo
-            })).length
-        }))).then(results => {
-            setData(results.map(result => ({ ...result, complexity: result.size * result.commitCount})).sort((file1, file2) => file2.complexity - file1.complexity))
-        })
-    }, []);
-    if (data.length === 0) {
-      return <Box>Loading</Box>
-    }
-    return (
-        <Box p={4}>
-            <Box
-                borderColor="border.default"
-                borderWidth={1}
-                borderStyle="solid"
-                borderRadius={6}
-                overflow="hidden"
-            >
-                <Box
-                    bg="canvas.subtle"
-                    p={3}
-                    borderBottomWidth={1}
-                    borderBottomStyle="solid"
-                    borderColor="border.default"
-                >
-                    This is the folder content.
-                </Box>
-                <Box p={4}>
-                    <table style={{textAlign: "left"}}>
-                        <thead>
-                        <tr>
-                            <th className="p-1">Path</th>
-                          <th className="p-1">Commit count</th>
-                          <th className="p-1">File size</th>
-                          <th className="p-1">Complexity</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {data.map(({path, commitCount, size, complexity}) => (
-                            <tr key={path}>
-                              <td className="p-1" onClick={() => props.onNavigateToPath(path)}>{path}</td>
-                              <td className="p-1">{commitCount}</td>
-                              <td className="p-1">{size}</td>
-                              <td className="p-1">{complexity}</td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
-                </Box>
-            </Box>
+  const [data, setData] = useState<
+    { path: string; commitCount: number; size: number; complexity: number }[]
+  >([]);
+  const {
+    context: { owner, repo },
+    onRequestGitHubEndpoint,
+    tree,
+  } = props;
+  useEffect(() => {
+    void Promise.all(
+      tree
+        .filter(
+          (item) => item.type === "blob" && item.path?.match(/\.(tsx?|jsx?)$/)
+        )
+        .map(async ({ path = "/", size }) => ({
+          path,
+          size: size ?? 0,
+          commitCount: (
+            await onRequestGitHubEndpoint("GET /repos/{owner}/{repo}/commits", {
+              path,
+              owner,
+              repo,
+            })
+          ).length,
+        }))
+    ).then((results) => {
+      setData(
+        results
+          .map((result) => ({
+            ...result,
+            complexity: result.size * result.commitCount,
+          }))
+          .sort((file1, file2) => file2.complexity - file1.complexity)
+      );
+    });
+  }, [owner, repo, onRequestGitHubEndpoint, tree]);
+  if (data.length === 0) {
+    return <Box>Loading</Box>;
+  }
+  return (
+    <Box p={4}>
+      <Box
+        borderColor="border.default"
+        borderWidth={1}
+        borderStyle="solid"
+        borderRadius={6}
+        overflow="hidden"
+      >
+        <Box
+          bg="canvas.subtle"
+          p={3}
+          borderBottomWidth={1}
+          borderBottomStyle="solid"
+          borderColor="border.default"
+        >
+          This is the folder content.
         </Box>
-    );
+        <Box p={4}>
+          <table style={{ textAlign: "left" }}>
+            <thead>
+              <tr>
+                <th className="p-1">Path</th>
+                <th className="p-1">Commit count</th>
+                <th className="p-1">File size</th>
+                <th className="p-1">Complexity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map(({ path, commitCount, size, complexity }) => (
+                <tr key={path}>
+                  <td
+                    className="p-1"
+                    onClick={() => props.onNavigateToPath(path)}
+                  >
+                    {path}
+                  </td>
+                  <td className="p-1">{commitCount}</td>
+                  <td className="p-1">{size}</td>
+                  <td className="p-1">{complexity}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
