3bbbc71969527bfbdede82aec8cd50e24b0d9d22
Daniel Bartholomae <daniel@bartholomae.name>
2023-03-06 23:46:27 +0800

    Don't show complexity explicitly

--- blocks/FileList.tsx
+++ blocks/FileList.tsx
@@ -14,12 +14,12 @@ export function FileList({
 
   return (
     <ActionList>
-      {files.map(({ path, commitCount, size, complexity }) => (
+      {files.map(({ path, commitCount, size }) => (
         <ActionList.Item key={path} onClick={() => onNavigateToPath(path)}>
           {path}
           <ActionList.TrailingVisual>
             {commitCount} commit{commitCount === 1 ? "" : "s"},{" "}
-            {(size / 1024).toFixed(0)} kB size, {complexity} complexity
+            {(size / 1024).toFixed(0)} kB size
           </ActionList.TrailingVisual>
         </ActionList.Item>
       ))}
