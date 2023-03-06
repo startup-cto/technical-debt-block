212abe2132fafe0a61415760e3d8c4eb978bbe41
Daniel Bartholomae <daniel@bartholomae.name>
2023-03-06 18:02:40 +0800

    Add test for calculating complexity

--- blocks/makeLoadTechDebt.test.ts
+++ blocks/makeLoadTechDebt.test.ts
@@ -37,6 +37,25 @@ describe("makeLoadTechDebt", () => {
     ).toEqual([]);
   });
 
+  it("calculates the complexity of a file", async () => {
+    const commitCount = 5;
+    const size = 10;
+    const complexity = commitCount * size;
+    const loadTechDebt = makeLoadTechDebt(() =>
+      Promise.resolve(Array.from({ length: commitCount }).fill({}))
+    );
+
+    expect(
+      await loadTechDebt([
+        {
+          type: "blob",
+          path: "/test.ts",
+          size,
+        },
+      ])
+    ).toContainEqual(expect.objectContaining({ complexity }));
+  });
+
   it("filters out non-code extensions", async () => {
     const loadTechDebt = makeLoadTechDebt(() => Promise.resolve([]));
 
